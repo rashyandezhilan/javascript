ec62c0e3e38a445ae14929b31bb0a12075ea3097
rashyandezhilan <drngp161it051s@gmail.com>
2022-07-03 14:08:52 +0530

    js is lreaning

---
+++ javascript/operator.js
@@ -0,0 +1,83 @@
+//arimathic operator//
+var v1 =40;
+var v2=20;
+//var sum= v1 + v2// 60
+//var sub= v1-v2//20
+//var mul= v1 * v2//800
+//var div= v1/v2//2
+//var pow =v1**v2//40*40=1600 (v1=40 ,v2=2)
+//var rem = v1 % v2//0
+console.log(" ----- assignment 0perator----")
+// assignment operator//
+var a = 10;
+//a +=20; // 10+ 20 = 30
+//a -= 20;//  -10 
+//a *= 5;// 50
+//a /= 2; // 5
+//a**=2; // 100
+//a %= 5; //  0 
+//var increament = ++a ; // 11
+// var increament = a++; //  10
+//var increament = --a; // 9
+ //var increament = a--; // 10
+
+ console.log(" -----  string  0perator----")
+
+ // string operator// 
+ var n =" marvels "
+ var name1=" movies "
+ //var movie= n + name1 // marvels movies
+ //var movie =n + " movies " // marvels movies
+ //console.log (movie)//
+
+
+ // comparsion operator//
+ var vi = 20 ;
+ var vr = '20 ' ;
+ //if ( vi== vr) {
+    // console.log (" sucessfully  match") // sucessfully match
+ //}
+
+ //if ( vi=== vr) { // unsucessfully match
+    // console.log (" sucessfully  match") 
+ //}
+ //else {
+   //  console.log( " unsucessfully match") 
+ //}
+ 
+
+
+
+ //  ternatry operator condition ? ///
+ //var outside = vi > 19? ' 20 is greater' : '20 is lesser' // condition ? //  20 is lesser
+  //var outside = vi > 19? ' 20 is greater' : '20 is lesser' // 20 is greater
+  //console.log( outside) 
+  
+  var x= 40;
+  var y =  60 ;
+  // ---- || or operator-----//
+  //if(log1||30 )
+  //{
+      //console.log("  match are true ")  // match  are true
+ // }
+ //---- and operator---//
+ //if(x == 40  && y == 60  )
+  // {
+   //  console.log(" yes  ")    //yes
+    // console.log(" both are correct") // both are correct
+ // }
+
+ // not  gate ///
+  if (x!== 40 || y!== 50) /// one is correct
+  {
+      console.log(" one is correct")
+  }
+  else
+  {
+      console.log(" both are wrong")
+  }
+  
+
+
+
+
---
+++ javascript/variable.js
@@ -0,0 +1,21 @@
+var a = " f" // datatype-  string
+var num = 30; // datatype- number
+var myData=undefined // datatype- undefined
+// obj={} key:value
+var m=[{ name:"dog"}] // datatype- object
+var isavail= true //datatype-  boolean
+var isavai= true||false //datatype- boolean
+var intnum = null;  // datatype- object
+var b= [ 1,2,2]//datatype-  string
+
+
+
+
+//console.log(a)//f
+//console.log(num)//30
+//console.log(name)// object
+//console.log (myData)//undefined
+//console.log(isavail)//true
+//console.log(isavai)//true
+ //console.log( typeof(intnum))
+ console.log(typeof( a))
